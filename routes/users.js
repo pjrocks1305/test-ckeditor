8b99134530eda124d40e5ea1f986145d13630746
Atul Joshi <atjoshi.net@gmail.com>
2017-08-17 17:08:15 +0530

    test check

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+    var a = 4;
+    res.send({data : a, status: 'respond with a resource'});
 });
 
 module.exports = router;
